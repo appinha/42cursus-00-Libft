92070a23927b880c8c965da4d5606debc523c371
appinha <amanda_pinha@hotmail.com>
2020-05-06 17:46:38 -0300

    Corrected CRLF to LF

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apuchill <apuchill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 18:07:59 by apuchill          #+#    #+#             */
-/*   Updated: 2020/02/03 14:40:58 by apuchill         ###   ########.fr       */
+/*   Updated: 2020/05/06 17:46:08 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void *ft_calloc(size_t count, size_t size)
 {
-	size_t	tot_size;
-	void	*dst;
+	size_t tot_size;
+	void   *dst;
 
 	tot_size = size * count;
 	if (!(dst = malloc(tot_size)))
