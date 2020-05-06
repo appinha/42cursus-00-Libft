711ba40cb065d644fcc066dc2cc349b8ce458ba1
appinha <amanda_pinha@hotmail.com>
2020-05-06 17:50:44 -0300

    Norminette test with spaces instead of tabulations

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,16 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 18:07:59 by apuchill          #+#    #+#             */
-/*   Updated: 2020/05/06 17:46:08 by apuchill         ###   ########.fr       */
+/*   Updated: 2020/05/06 17:50:15 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	size_t tot_size;
-	void   *dst;
+	size_t	tot_size;
+	void	*dst;
 
 	tot_size = size * count;
 	if (!(dst = malloc(tot_size)))
